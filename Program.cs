8266ad845b0ecd2af4ae49a74942754947ccb035
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2022-10-08 17:09:40 +0800

    решила задачу 15

--- Program.cs
+++ Program.cs
@@ -3,4 +3,18 @@
 // 7 -> да
 // 1 -> нет
 Console.Clear();
-Console.Write("Введите число:");
+Console.Write("Введите номер дня недели: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num < 8)
+{
+    if (num < 6)
+    {
+        Console.WriteLine("Будний день");
+    }
+    else
+    {
+        Console.WriteLine("Выходной день");
+    }
+}
+else 
+Console.WriteLine("СЕМЬ ДНЕЙ!");
